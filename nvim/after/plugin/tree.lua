40b9e1abb186dce3b9cc1ed3a6d67acf711b1bd4
Hugo Giles <hugogiles51@gmail.com>
2023-03-31 14:39:23 +1100

    up

--- nvim/after/plugin/tree.lua
+++ nvim/after/plugin/tree.lua
@@ -1,22 +1,20 @@
--- examples for your init.lua
-
--- disable netrw at the very start of your init.lua (strongly advised)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
 
--- OR setup with some options
+vim.opt.termguicolors = true
 require("nvim-tree").setup({
     sort_by = "case_sensitive",
     renderer = {
         group_empty = true,
     },
     filters = {
-        dotfiles = true,
+        dotfiles = true,    
     },
 })
+
+-- Keymaps  
+vim.keymap.set("n", "<leader>bb", vim.cmd.NvimTreeToggle)
 
 local function open_nvim_tree()
     require("nvim-tree.api").tree.open()
--- nvim/lua/hugo/remap.lua
+++ nvim/lua/hugo/remap.lua
@@ -1,2 +1,12 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.api.nvim_set_keymap('n', "<leader>pv", ":bp | sp | bn | bd<CR>", { noremap = true, silent = true })
+
+-- windows
+vim.api.nvim_set_keymap('n', "<leader>wh", "<C-w>h", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', "<leader>wl", "<C-w>l", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', "<leader>wj", "<C-w>j", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', "<leader>wk", "<C-w>k", { noremap = true, silent = true })
+
+-- buffers
+vim.api.nvim_set_keymap('n', "<leader>bh", ":bprevious<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', "<leader>bl", ":bnext<CR>", { noremap = true, silent = true })
